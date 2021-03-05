4a963e8cc5da172c1f0627dda6340088a800a8db
YUKIHISAMATSUOKA <amx004qubeley105@gmail.com>
2021-03-05 00:39:06 +0000

    地域実装

---
+++ app/models/area.rb
@@ -0,0 +1,6 @@
+class Area < ApplicationRecord
+
+  validates :name, presence: true
+  validates :name, length: { maximum: 20}
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,6 @@ Rails.application.routes.draw do
 
   resources :genres, only: [:create, :index, :edit, :update, :destroy]
   get '/genres/:id' => 'genres#index' #editでバリデエラー発生後に、No route matches [GET] "/genres/1"となったので付け足した
+  resources :areas, only: [:new, :create, :index, :edit, :update, :destroy]
 
 end

793cef0fe1e503eb1c650bfc93eeb222367fa11d
YUKIHISAMATSUOKA <amx004qubeley105@gmail.com>
2021-03-08 23:58:17 +0000

    画像トリミング実装前

--- app/models/item.rb
+++ app/models/item.rb
@@ -7,6 +7,7 @@ class Item < ApplicationRecord
   validates :name, length: { maximum: 30}
   validates :introduction, length: { maximum: 50}
   validates :price, presence: true
+  validates :cook_time, presence: true
 
   # enum cook_time:[:"5分", :"10分", :"15分", :"20分", :"25分", :"30分"]
 end

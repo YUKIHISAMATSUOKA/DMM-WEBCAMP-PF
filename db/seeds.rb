7ba549167c4158be46de9097fc3e7ed4c97384bd
choco-monaka <amx004qubeley105@gmail.com>
2021-03-19 07:28:26 +0000

    aaa

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+  5.times do |n|
+    Customer.create!(
+      last_name: "サブロー#{n + 1}",
+      first_name: "テスト#{n + 1}",
+      kana_last_name: "テスト#{n + 1}",
+      kana_first_name: "テスト#{n + 1}",
+      member_state: "true",
+      email: "test@test#{n + 1}",
+      password: "111222"
+    )
+  end

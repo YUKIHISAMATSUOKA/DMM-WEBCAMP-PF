c77c7dd95800467c86ed63d1cad854c89c676028
choco-monaka <amx004qubeley105@gmail.com>
2021-03-21 07:09:23 +0000

    お問い合わせい昨日

--- app/mailers/inquiry_mailer.rb
+++ app/mailers/inquiry_mailer.rb
@@ -1,6 +1,5 @@
 class InquiryMailer < ApplicationMailer
 
-  # default to: "example@example.com"   # 送信元アドレス
   # 環境ファイルにすることで、お問い合わせしたの内容が、ちゃんと自分のsend_mailで設定したアドレスに届く
   default to: ENV['SEND_MAIL'] # 送信元アドレス
 
@@ -8,5 +7,6 @@ class InquiryMailer < ApplicationMailer
     @inquiry = inquiry
     mail(:from => inquiry.email, :subject => 'お問い合わせがあったよ！')
   end
+
 
 end

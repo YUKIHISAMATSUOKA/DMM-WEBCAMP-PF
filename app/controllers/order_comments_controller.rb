c514fdf7b80779002ce7f54338824bc87382a7d4
choco-monaka <amx004qubeley105@gmail.com>
2021-03-25 13:45:38 +0000

    コメント機能非同期

--- app/controllers/order_comments_controller.rb
+++ app/controllers/order_comments_controller.rb
@@ -4,13 +4,18 @@ class OrderCommentsController < ApplicationController
     @order = Order.find(params[:order_id])
     @order_comment = current_customer.order_comments.new(order_comment_params)
     @order_comment.order_id = @order.id
-    @order_comment.save!
-    redirect_to order_path(@order)
+    @order_comment.save
+    # 非同期化に伴い削除
+    # redirect_to order_path(@order)
   end
 
   def destroy
-    OrderComment.find_by(id: params[:id], order_id: params[:order_id]).destroy
-    redirect_to order_path(params[:order_id])
+    # OrderComment.find_by(id: params[:id], order_id: params[:order_id]).destroy
+    @order = Order.find(params[:order_id])
+    @order_comment = @order.order_comments.find(params[:id])
+    @order_comment.destroy
+    # 非同期化に伴い削除
+    # redirect_to order_path(params[:order_id])
   end
 
   private

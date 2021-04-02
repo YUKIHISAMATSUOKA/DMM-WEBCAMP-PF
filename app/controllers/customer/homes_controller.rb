7f35d010ec72d4248618d2487a07bf07b7a8d12c
choco-monaka <amx004qubeley105@gmail.com>
2021-04-02 02:18:03 +0000

    細かい修正

--- app/controllers/customer/homes_controller.rb
+++ app/controllers/customer/homes_controller.rb
@@ -2,10 +2,10 @@ class Customer::HomesController < ApplicationController
 
   def top
     @shops = Shop.all
-    # 新着ショップ４つ
+    # 新着ショップ４つ あとでswiperを使用したい
     @shop = Shop.where(status: "true").limit(4).order(" created_at DESC ")
   end
-
+  
   def service
   end
 
